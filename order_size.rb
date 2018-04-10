076a390560f2eef5d1d0757c3ced4e90122f24f7
Dion Chan <hwchan91@gmail.com>
2018-04-10 18:33:14 +0800

    add BipolarComparison

--- order_size.rb
+++ order_size.rb
@@ -109,6 +109,7 @@ class Node
     @attributes = []
   end
 
+  # TO DO: need to skip adding again if the relationship already exists
   def add_comparison(attribute_name:, relationship:, other_node:)
     sequence = find_sequence(attribute_name)
     sequence.send(relationship) << other_node # relationship: "less_than" OR "greater_than"
@@ -129,6 +130,35 @@ class Node
   end
 end
 
+class BipolarComparison
+  attr_accessor :name, :lesser, :greater
+
+  def initialize(opt = {})
+    @name = opt[:name]
+    @lesser = opt[:lesser]
+    @greater = opt[:greater]
+  end
+
+  def self.all
+    ObjectSpace.each_object(self).to_a
+  end
+
+  def self.count
+    all.length
+  end
+
+  def self.identify(symbol)
+    self.all.find{|c| [c.lesser, c.greater].include? symbol }
+  end
+
+  def in_words(symbol, is_front)
+    if symbol == lesser && is_front || symbol == greater && !is_front
+      'less_than'
+    else
+      'greater_than'
+    end
+  end
+end
 
 class Field
   attr_accessor :nodes
@@ -138,24 +168,23 @@ class Field
   end
 
   def parse(statement)
-    symbol = statement.split("").find{|i| ["<", ">"].include? i }
-    node_names = statement.split(symbol)
-    found_nodes = node_names.map {|node_name| find_or_create_node(node_name) }
+    symbols = statement.split(" ")
+    comparison = nil
+    comparison_symbol = symbols.find{ |symbol| comparison = BipolarComparison.identify(symbol) }
+    found_nodes = node_names(symbols, comparison_symbol).map {|node_name| find_or_create_node(node_name) }
 
     #check_if_valid (i.e. not: A<B, B<C, C<A)
 
     found_nodes.each_with_index do |node, index|
       is_front = index == 0
-      node.add_comparison(attribute_name: "numerical_value", relationship: relationship(symbol, is_front), other_node: other_node(found_nodes, is_front))
+      node.add_comparison(attribute_name: comparison.name, relationship: comparison.in_words(comparison_symbol, is_front), other_node: other_node(found_nodes, is_front))
     end
   end
 
-  def relationship(symbol, is_front)
-    if symbol == '<' && is_front || symbol == '>' && !is_front
-      'less_than'
-    else
-      'greater_than'
-    end
+  # get the names immediately before and immediately after the comparison symbol
+  def node_names(symbols, comparison_symbol)
+    index = symbols.index(comparison_symbol)
+    [symbols[index-1], symbols[index +1] ]
   end
 
   def other_node(found_nodes, is_front)
@@ -168,10 +197,19 @@ class Field
   end
 end
 
+
+# predefine all comparisons that are used
+BipolarComparison.new(name: "numerical_value", lesser: "<", greater: ">")
+
 s = Field.new
-s.parse('a<b')
-s.parse('a<c')
-s.parse('c<d')
-s.parse('c<e')
+s.parse('a < b')
+s.parse('a < c')
+s.parse('c < d')
+s.parse('c < e')
+s.parse('e < f')
+s.parse('e < g')
+s.parse('e < h')
+s.parse('b > i')
+s.parse('i < j')
 a = s.nodes['a']
 a.attributes.find{|a| a.name == 'numerical_value'}.sequence.get_trails_less_than_names
